77d6c2b675b156329f022e27aea730dccf34dc3d
DevJonasMax <jonas.max.dev@gmail.com>
2026-02-12 14:26:25 -0400

    update user service, controller

--- apps/api/src/users/users.controller.ts
+++ apps/api/src/users/users.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, Patch, Body } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { UpdateUserDto } from './dtos/update.dto';
 
 @Controller('users')
 export class UsersController {
@@ -11,5 +12,9 @@ export class UsersController {
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.usersService.findOne(id);
+  }
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+    return this.usersService.update(id, updateUserDto);
   }
 }
